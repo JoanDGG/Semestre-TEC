abb0acd57fd203d7658be895a6ba063d05c17396
JoanDGG <ggjoan2000@gmail.com>
2022-08-11 17:07:42 -0500

    Solar system update

--- Modelado3D/Assets/Scripts/Rotacion.cs
+++ Modelado3D/Assets/Scripts/Rotacion.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class Rotacion : MonoBehaviour
 {
     public Vector3 rotateValues = new Vector3(1f, 0f, 3f);
+    public Transform pivot;
+    public Vector3 axis = new Vector3(0, 1, 0);
+    public float rotateVelocity = 1f;
+
 
     void Update()
     {
         RotateSphere();
+        
+        if(pivot != null)
+            RotateAroundSphere();
     }
     void RotateSphere()
     {
-        //gameObject.transform.localRotation = Quaternion.Euler(rotateValues.x * Time.deltaTime, rotateValues.y * Time.deltaTime, rotateValues.z * Time.deltaTime);
         gameObject.transform.Rotate(rotateValues.x, rotateValues.y, rotateValues.z, Space.Self);
+    }
+    void RotateAroundSphere()
+    {
+        gameObject.transform.RotateAround(pivot.position, axis, rotateVelocity);
     }
 }
